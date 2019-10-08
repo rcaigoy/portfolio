2301a160cd1e4233c624ea7a3a5cde1124efa612
rc13267 <ryan.caigoy@appliedmedical.com>
2019-10-08 14:21:41 -0700

    adds hamburger menu in mobile

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -2,19 +2,50 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Menu.css';
 
-function Menu() {
-    return (
-        <div className="Menu">
+var show = false;
+var MenuLink = 'Menu-Link';
+
+class Menu extends React.Component{
+
+    
+    constructor() {
+        super()
+        this.state = {
+            MenuLink: 'Menu-Link',
+            show: false
+        }
+    }
+
+    ShowMenu(){
+        if (this.state.show){
+            this.setState({show: false, MenuLink: 'Menu-Link'});
+        }
+        else {
+            this.setState({show: true, MenuLink: 'Menu-Link-Hidden'});
+        }
+        //alert(MenuLink);
+    }
+
+    render() {
+        return(
+            <div className="Menu">
             <Link to="/" className="Logo">Ryan Caigoy</Link>
 
             <div className="Menu-Navigation">
-                <Link to="/" className="Menu-Link">Home</Link>
-                <a href="/Interactive-Portfolio" className="Menu-Link">
+                <div className="Hamburger" 
+                    onClick={() => this.ShowMenu(show)}
+                >
+                    <i className="fas fa-bars fa-2x"></i>
+                </div>
+                <Link to="/" className={this.state.MenuLink}
+                >Home</Link>
+                <a href="/Interactive-Portfolio" className={this.state.MenuLink}>
                     Interactive Portfolio
                 </a>
             </div>
         </div>
-    );
+        );
+    }
 }
 
 export default Menu;
